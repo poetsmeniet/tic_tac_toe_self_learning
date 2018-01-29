15d82b091e70215085aa4b56ec8713be24e5f0ae
Thomas <thomas@geenbs.nl>
2018-01-29 16:54:19 +0100

    Implemented initGame

--- keeper.c
+++ keeper.c
@@ -5,41 +5,26 @@
 #include <string.h> 
 #include <unistd.h>
 
+/* The game state:
+ * * = empty 
+ * O = Player O position
+ * X = Player X position
+ */
 typedef struct gameArrayInt{
-    int game;
+    char game[10];
 }gInt;
+
+struct gameArrayInt *initGame(char *name);
 
 int main(void)
 {
-    char name[] = "/sharedmem1";
-    int smfd = shm_open (name, O_RDWR | O_CREAT, 0660); 
+    char name[] = "/gameState";
 
-    if(smfd == -1){
-        perror("Unable to open shared memory\n");
-        shm_unlink(name);
-        return 1;
-    }
-
-    struct gameArrayInt *p;
-    p = mmap(NULL, sizeof(struct gameArrayInt), PROT_WRITE, MAP_SHARED, smfd, 0);
-
-    if (ftruncate (smfd, sizeof (struct gameArrayInt)) == -1) 
-        perror ("ftruncate");
-
-    if(p == -1){
-        perror("mmap failed.\n");
-        munmap(&p, sizeof(int));
-        return 1;
-    }
-
-    //test
-    p->game = 13;
-    printf("p->game = %i\n", p->game);
-    
+    struct gameArrayInt *p = initGame(name);
     sleep(5);
 
-    p->game = 135;
-    printf("p->game = %i\n", p->game);
+    memcpy(p->game, "*O*******\0", 10);
+    printf("p->game = %s\n", p->game);
 
     sleep(5);
 
@@ -48,4 +33,34 @@ int main(void)
     shm_unlink(name);
 
     return 0;
+}
+
+/* Returns pointer to shared memory game state array */
+struct gameArrayInt *initGame(char *name)
+{
+    int smfd = shm_open (name, O_RDWR | O_CREAT, 0660); 
+
+    if(smfd == -1){
+        perror("Unable to open shared memory\n");
+        shm_unlink(name);
+        return NULL;
+    }
+
+    struct gameArrayInt *p;
+    p = mmap(NULL, sizeof(struct gameArrayInt), PROT_WRITE, MAP_SHARED, smfd, 0);
+
+    if (ftruncate (smfd, sizeof (struct gameArrayInt)) == -1) 
+        perror ("ftruncate");
+
+    if(p < 0){
+        perror("mmap failed.\n");
+        munmap(&p, sizeof(int));
+        return NULL;
+    }
+
+    //Init new gameState
+    memcpy(p->game, "*********\0", 10);
+    printf("p->game = %s\n", p->game);
+
+    return p;
 }
