88d9cb3e4b4a4d41c13e6b45f8483f5fae0eabba
Thomas <thomas@geenbs.nl>
2018-04-14 20:05:15 +0200

    Added returnAvailableMoves function

--- minimax.c
+++ minimax.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 
 int isWinning(char *gameState, char currPlayer);
+
+void returnAvailableMoves();
     
 int main(void)
 {
-
-    char gameState[] = "OOO******";
-    char currPlayer = 'O';
+    /* Define some test variables */
+    char gameState[] = "OOO**O**X";
+    char currPlayer = 'X';
     printf("gamestate '%s', does player '%c' win: %d\n", gameState, currPlayer, isWinning(gameState, currPlayer));
+
+    /* Return and loop available moves
+     * - Run minimal for every avMove */
+    int avMoves[8];
+    int i = 0;
+    returnAvailableMoves(gameState, avMoves);
+    while(avMoves[i] != -1)
+    {
+        printf("avMove: %d\n", avMoves[i]); 
+        i++;
+    }
+
     return 0;
 }
 
@@ -28,4 +42,19 @@ int isWinning(char *gameState, char currPlayer)
         return 1;
     else
         return 0;
+}
+
+/* Fills array of ints with indices of available moves */
+void returnAvailableMoves(char *gameState, int *avMoves)
+{
+    int i;
+    int j = 0;
+    for(i = 0; i < 9; i++){
+        if(gameState[i] == '*'){
+            avMoves[j] = i;
+            j++;
+        }
+    }
+
+    avMoves[j] = -1;
 }
