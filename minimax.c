aaaeaca6dfec07ed642ef0cf244cd89800340517
Thomas <thomas@geenbs.nl>
2018-04-14 22:37:10 +0200

    Plugged minimax algorithm into simple game client

--- minimax.c
+++ minimax.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h> 
+#include <fcntl.h> 
+#include <string.h> 
+#include <unistd.h>
+#include "tictactoe.h"
+#define CLEAR() printf("\033[H\033[J") //to clear the linux term
+
+void displayGame(gData *p, char player);
 
 struct movesInfo{
     int index;
@@ -7,8 +15,8 @@ struct movesInfo{
 };
 
 /* Set whom is whom */
-char aiPlayer = 'X';
-char bioPlayer = 'O';
+char aiPlayer = 'O';
+char bioPlayer = 'X';
 
 int isWinning(char *gameState, char currPlayer);
 
@@ -20,17 +28,100 @@ int selectBestMove(struct movesInfo *moves);
     
 int main(void)
 {
-    /* Define some test variables */
-    char gameState[] = "XOX**O**X";
-    struct movesInfo moves[1000000];
     
-    /* Run minimax */
-    miniMax(gameState, aiPlayer, moves);
-    
-    /* Select best move */
-    int bestMove = selectBestMove(moves);
-    printf("Index chosen for best move: %d\n", bestMove);
-    
+    char name[] = "/gameState";
+    int smfd = shm_open (name, O_RDWR, 0660); 
+
+    if(smfd == -1){
+        perror("Unable to open shared memory\n");
+        return 1;
+    }
+
+    gData *p;
+    p = mmap(NULL, sizeof(gData), PROT_WRITE, MAP_SHARED, smfd, 0);
+
+    if(p < 0){
+        perror("mmap failed.\n");
+        return 1;
+    }
+
+    printf("p->game read from master process = %s\n", p->game);
+
+    char player;
+    printf("Trying to be player X..\n");
+    if(p->playerX == '*'){
+        p->playerX = 'X';
+        player = 'X';
+    }else if(p->playerO == '*'){
+        p->playerO = 'O';
+        player = 'O';
+    }else{
+        printf("No available player slots\n");
+        return 1;
+    }
+    printf("This instance is player %c\n", player);
+
+    /* Main loop */
+    while(1){
+        char nextMove[10];
+        //Turn?
+        if(p->playerTurn == player && p->lck){
+            displayGame(p, player);
+            
+            //char gameState[] = "XOX**O**X";
+            char gameState[10];
+            memcpy(gameState, p->game, 9);
+            gameState[9] = '\0';
+            printf("!! copied gameState: '%s'. pointer gs: '%s'\n", gameState, p->game);
+            struct movesInfo moves[9];
+            
+            /* Run minimax */
+            printf("calling minimax with gamestate: '%s'\n", gameState);
+            miniMax(gameState, aiPlayer, moves);
+            
+            /* Select best move */
+            int bestMove = selectBestMove(moves);
+            printf("Index chosen for best move: %d\n", bestMove);
+            
+            if(bestMove == 0)
+                memcpy(nextMove, "a1", 2);
+            else if(bestMove == 1)
+                memcpy(nextMove, "b1", 2);
+            else if(bestMove == 2)
+                memcpy(nextMove, "c1", 2);
+            else if(bestMove == 3)
+                memcpy(nextMove, "a2", 2);
+            else if(bestMove == 4)
+                memcpy(nextMove, "b2", 2);
+            else if(bestMove == 5)
+                memcpy(nextMove, "c2", 2);
+            else if(bestMove == 6)
+                memcpy(nextMove, "a3", 2);
+            else if(bestMove == 7)
+                memcpy(nextMove, "b3", 2);
+            else if(bestMove == 8)
+                memcpy(nextMove, "c3", 2);
+
+            printf("nextMove selected: '%s'\n", nextMove);
+
+            p->lck = 0; //lock mem
+            memcpy(p->nextMove, nextMove, 3);
+            p->nextMove[2] = '\0';
+
+            /* Truncate moves.. */
+            int i;
+            for(i = 0; i < 9; i++){
+                moves[i].index = 0;
+                moves[i].score = 0;
+            }
+
+        }else{
+            displayGame(p, player);
+        }
+        usleep(50000);
+    }
+
+    munmap(&p, sizeof(gData));
     return 0;
 }
 
@@ -71,16 +162,15 @@ int returnAvailableMoves(char *gameState, int *avMoves)
 
 int miniMax(char *gameState, char player, struct movesInfo *moves)
 {
-
     /* Return available moves */
     int avMoves[8];
     int i = 0;
     int avMvCnt = returnAvailableMoves(gameState, avMoves);
     
     /* Base case: return terminal gameState; win, lose, tie */
-    if(isWinning(gameState, aiPlayer))
+    if(isWinning(gameState, bioPlayer))
         return 1;
-    else if(isWinning(gameState, bioPlayer))
+    else if(isWinning(gameState, aiPlayer))
         return -1;
     else if(avMvCnt == 0)
         return 0;
@@ -123,12 +213,35 @@ int selectBestMove(struct movesInfo *moves)
     int currBest = 0;
     int bestIndex;
     for(i = 0; i < 9; i++){
-        //printf("\tA score of %d is found, with index: %d\n", moves[i].score, moves[i].index);
-        if(moves[i].score > currBest){
-            //printf("\t\tSetting highest score of %d is found, with index: %d\n", moves[i].score, moves[i].index);
+        if(moves[i].score >= currBest){
             bestIndex = moves[i].index;
             currBest = moves[i].score;
         }
     }
+
+    /* If no best move, select first available move */
+    if(currBest == 0)
+    for(i = 0; i < 9; i++)
+        if(moves[i].index > 0)
+            bestIndex = moves[i].index;
+
     return bestIndex;
+}
+
+void displayGame(gData *p, char player)
+{
+    CLEAR();
+    printf("Player: %c\n\n", player);
+
+    int i;
+    int j = 9;
+    
+    for(i = 3; i > 0; i--){
+        printf("%i    %c | %c | %c \n", i, p->game[j - 3], p->game[j - 2], p->game[j - 1]);
+        if(i > 1)
+            printf("    ---|---|---\n");
+        j -= 3;
+    }
+    printf("\n     a   b   c\n\n");
+    printf("%s\n", p->bcastMsg);
 }
